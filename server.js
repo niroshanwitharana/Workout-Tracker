01c20d93c64b3ad912a69785ae57ee643fa3d01f
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-07-28 23:08:34 +0100

    set up port and required packages

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
+
 //set a port
 const PORT = process.env.PORT || 8080;
 
@@ -11,7 +12,15 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
+//roots setup
+require('./routes/api-routes')(app);
+require('./routes/html-routes')(app);
 
 //setting up database URL
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//listening to the PORT
+app.listen(PORT , ()=>{
+    console.log(`app running on ${PORT}`);
+});
 
